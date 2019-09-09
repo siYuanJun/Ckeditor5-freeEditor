03a81a9486466bb8ecfbf6e1fef9c999f6cd6fc1
siYuanJun <2039750417@qq.com>
2019-09-10 00:15:53 +0800

    fc

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/* eslint-env node */
+
+'use strict';
+
+module.exports = {
+	extends: 'ckeditor5',
+	rules: {
+		"linebreak-style": [0 ,"error", "windows"]
+	}
+};
+
